649fddcfa33e61b40285a3d76ad9338d4883cbff
bosco227 <antoniobosco2612@gmail.com>
2026-03-11 16:04:25 -0300

    Correção de bugs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,38 +1,48 @@
+import { motion } from "framer-motion";
+
 export default function Footer() {
   return (
-    <footer className="mt-32 border-t border-slate-800 py-10">
-      <div className="max-w-6xl mx-auto px-6 text-center text-gray-400">
-        <h3 className="text-white font-semibold text-lg">Antônio Bosco</h3>
+    <motion.section
+      id="projects"
+      className="py-20 scroll-mt-24"
+      initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+    >
+      <footer className="mt-32 border-t border-slate-800 py-10">
+        <div className="max-w-6xl mx-auto px-6 text-center text-gray-400">
+          <h3 className="text-white font-semibold text-lg">Antônio Bosco</h3>
 
-        <div className="flex justify-center gap-6 mt-4 text-sm">
-          <a
-            href="https://github.com/bosco227"
-            target="_blank"
-            className="hover:text-sky-400 transition"
-          >
-            GitHub
-          </a>
+          <div className="flex justify-center gap-6 mt-4 text-sm">
+            <a
+              href="https://github.com/bosco227"
+              target="_blank"
+              className="hover:text-sky-400 transition"
+            >
+              GitHub
+            </a>
 
-          <a
-            href="https://www.linkedin.com/in/antonio-bosco-720721362/"
-            target="_blank"
-            className="hover:text-sky-400 transition"
-          >
-            LinkedIn
-          </a>
+            <a
+              href="https://www.linkedin.com/in/antonio-bosco-720721362/"
+              target="_blank"
+              className="hover:text-sky-400 transition"
+            >
+              LinkedIn
+            </a>
 
-          <a
-            href="mailto:antoniobosco2612@gmail.com"
-            className="hover:text-sky-400 transition"
-          >
-            Email
-          </a>
+            <a
+              href="mailto:antoniobosco2612@gmail.com"
+              className="hover:text-sky-400 transition"
+            >
+              Email
+            </a>
+          </div>
+
+          <p className="text-xs mt-6 text-gray-500">
+            © {new Date().getFullYear()} Antônio Bosco
+          </p>
         </div>
-
-        <p className="text-xs mt-6 text-gray-500">
-          © {new Date().getFullYear()} Antônio Bosco
-        </p>
-      </div>
-    </footer>
+      </footer>
+    </motion.section>
   );
 }
