12acb7562c24e743fcb8e89295c5fa25c98fdef5
bosco227 <antoniobosco2612@gmail.com>
2026-03-11 16:05:28 -0300

    correção de bugs parte 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export default function Footer() {
   return (
     <motion.section
-      id="projects"
+      id="contacts"
       className="py-20 scroll-mt-24"
       initial={{ opacity: 0, y: 40 }}
       whileInView={{ opacity: 1, y: 0 }}
